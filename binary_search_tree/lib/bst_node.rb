e182f520a3404c1806a8903bd21cf3dcede90faa
jacob <jbutler8@terpmail.umd.edu>
2018-01-08 15:13:56 -0800

    clean up bst_node class

--- binary_search_tree/lib/bst_node.rb
+++ binary_search_tree/lib/bst_node.rb
@@ -8,11 +8,4 @@ class BSTNode
     @right = nil
   end
 
-  def insert(value)
-    if value <= @value
-      @left.nil? ? @left = BSTNode.new(value) : @left.insert(value)
-    else
-      @right.nil? ? @right = BSTNode.new(value) : @right.insert(value)
-    end
-  end
 end
