6a8ab1b5e189dd05d13c7660ac6fc0c998f3854c
jacob <jbutler8@terpmail.umd.edu>
2018-02-25 18:41:26 -0800

    complete Tarjan's algorithm

--- topological_sort/lib/topological_sort.rb
+++ topological_sort/lib/topological_sort.rb
@@ -3,74 +3,86 @@ require_relative 'graph'
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 # Kahn's
-def topological_sort(vertices)
-  sorted = []
-  in_count = {}
-  queue = []
-
-  vertices.each do |vertex|
-    in_count[vertex] = in_edge_hash(vertex)
-
-    queue.push(vertex) if in_count[vertex] == 0
-  end
-
-  until queue.empty?
-    current = queue.shift
-    sorted << current
-
-    current.out_edges.each do |edge|
-
-      in_count[edge.to_vertex] -= 1
-
-      queue.push(edge.to_vertex) if in_count[edge.to_vertex] == 0
-    end
-
-    vertices.delete(current)
-  end
-
-  return vertices.empty? ? sorted : []
-end
-
-
-# Tarjan's
-# def topological_sort(graph)
-#   output = []
-#   visited = Hash.new(false)
+# def topological_sort(vertices)
+#   sorted = []
+#   in_count = {}
 #   queue = []
 #
-#   graph.each do |vertex|
+#   vertices.each do |vertex|
+#     in_count[vertex] = in_edge_hash(vertex)
 #
-#     if in_edge_hash(vertex) == 0
-#       queue.push(vertex)
-#     end
+#     queue.push(vertex) if in_count[vertex] == 0
 #   end
 #
 #   until queue.empty?
 #     current = queue.shift
+#     sorted << current
 #
 #     current.out_edges.each do |edge|
-#       if !visit(edge.to_vertex, output, visited)
 #
-#       end
+#       in_count[edge.to_vertex] -= 1
 #
+#       queue.push(edge.to_vertex) if in_count[edge.to_vertex] == 0
+#     end
+#
+#     vertices.delete(current)
 #   end
 #
-#
-#
-# end
-#
-# # helper methods
-# def visit(current_node, prev_node, leaves, visited)
-#   if current_node.out_edges.empty? && !visited[current_node]
-#     leaves.unshift(current_node)
-#
-#     visited[current_node] = true
-#   else
-#     false
-#   end
+#   return vertices.empty? ? sorted : []
 # end
 
-def in_edge_hash(vertex)
-  count = 0
-  vertex.in_edges.length
+
+# Tarjan's
+def topological_sort(vertices)
+  ordering = []
+  explored = Set.new
+
+  vertices.each do |vertex| # O(|v|)
+    dfs!(vertex, explored, ordering) unless explored.include?(vertex)
+  end
+
+  ordering
+end
+
+def dfs!(vertex, explored, ordering)
+  explored.add(vertex)
+
+  vertex.out_edges.each do |edge| # O(|e|)
+    new_vertex = edge.to_vertex
+    dfs!(new_vertex, explored, ordering) unless explored.include?(new_vertex)
+  end
+
+  ordering.unshift(vertex)
+end
+
+# Tarjans with cycle catching
+def topological_sort(vertices)
+  order = []
+  explored = Set.new
+  temp = Set.new
+  cycle = false
+
+  vertices.each do |vertex|
+    cycle = dfs!(vertex, explored, temp, order, cycle)  unless explored.include?(vertex)
+    return [] if cycle
+  end
+
+  order
+end
+
+
+def dfs!(vertex, explored, temp, order, cycle)
+  return true if temp.include?(vertex)
+  temp.add(vertex)
+
+  vertex.out_edges.each do |edge|
+    next_vertex = edge.to_vertex
+    cycle = dfs!(next_vertex, explored, temp, order, cycle) unless explored.include?(next_vertex)
+    return true if cycle
+  end
+
+  explored.add(vertex)
+  temp.delete(vertex)
+  order.unshift(vertex)
+  false
 end
